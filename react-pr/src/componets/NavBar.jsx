4dfd4626c8a74ddf37035614b6b90d53fee84c58
baltasar benitez <baltaarex363@gmail.com>
2024-02-09 15:56:38 -0300

    cambios de clases en NavBar

--- react-pr/src/componets/NavBar.jsx
+++ react-pr/src/componets/NavBar.jsx
@@ -2,9 +2,9 @@ import { Link, NavLink } from "react-router-dom";
 import CarWidget from "./CarWidget";
 
 const NavBar = () => {
-    return (
-        <div className="row">
-            <div className="col">
+    return(
+        <>
+            <div className="row">
                 <ul className="nav">
                     <li className="nav-item">
                         <NavLink className="nav-link text-light fs-5" to={"/contenido"}>LO MÁS NUEVO</NavLink>
@@ -18,12 +18,12 @@ const NavBar = () => {
                     <li className="nav-item">
                         <NavLink className="nav-link text-light fs-5" to={"/category/accesorios"}>ACCESORIOS</NavLink>
                     </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link text-dark fs-5" to={"#"}><CarWidget /></NavLink>
+                    </li>
                 </ul>
             </div>
-            <div className="col-md-1 d-flex align-items-center justify-content-end text-end">
-                <NavLink className="nav-link text-dark fs-5" to={"#"}><CarWidget /></NavLink>
-            </div>
-        </div>
+        </>
     )
 }
 
