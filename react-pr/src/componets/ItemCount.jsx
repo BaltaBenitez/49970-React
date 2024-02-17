7577be1b01406932077d8f09ef4ad0666e9ee2d3
baltasar benitez <baltaarex363@gmail.com>
2024-02-16 22:37:43 -0300

    ItemCount sin btn basura

--- react-pr/src/componets/ItemCount.jsx
+++ react-pr/src/componets/ItemCount.jsx
@@ -28,20 +28,20 @@ const ItemCount = ({stock}) => {
         <>
             <div className="row my-1 text-center">
                 <div className="col">
-                    <div className="btn-group btn-group-md me-3" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-danger" onClick={decrementar}>-</button>
-                        <button type="button" className="btn btn-danger px-3">{counter}</button>
-                        <button type="button" className="btn btn-danger" onClick={incrementar}>+</button>
-                    </div>
                     <div className="btn-group btn-group-md" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-warning px-3" >BASURA</button>
+                        <button type="button" className="btn btn-danger px-3" onClick={decrementar}>-</button>
+                        <button type="button" className="btn btn-danger px-4">{counter}</button>
+                        <button type="button" className="btn btn-danger px-3" onClick={incrementar}>+</button>
                     </div>
+                    {/* <div className="btn-group btn-group-md" role="group" aria-label="Basic example">
+                        <button type="button" className="btn btn-warning px-3" >BASURA</button>
+                    </div> */}
                 </div>
             </div>
             <div className="row my-1 text-center">
                 <div className="col">
                     <div className="btn-group btn-group-md" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-danger px-5" onClick={onAdd}>Agregar al Carrito</button>
+                        <button type="button" className="btn btn-danger" onClick={onAdd}>Agregar al Carrito</button>
                     </div>
                 </div>
             </div>
