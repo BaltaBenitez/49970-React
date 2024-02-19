647faf4a24bf5abe793f7a54a4a42e33d55f14f3
baltasar benitez <baltaarex363@gmail.com>
2024-02-19 15:32:30 -0300

    className

--- react-pr/src/componets/Footers/SocialMedia.jsx
+++ react-pr/src/componets/Footers/SocialMedia.jsx
@@ -1,9 +1,9 @@
 const SocialMedia = () => {
     return(
         <li className="nav-item fs-5 ">
-            <a className="nav-link d-inline p-2 text-decoration-none" href="https://www.facebook.com/redbullshop/" target="_blank" rel="noopener noreferrer"><i class="bi bi-facebook text-light"></i></a>
-            <a className="nav-link d-inline p-2 text-decoration-none" href="https://www.instagram.com/redbullshop/" target="_blank" rel="noopener noreferrer"><i class="bi bi-instagram text-light"></i></a>
-            <a className="nav-link d-inline p-2 text-decoration-none" href="https://twitter.com/redbullshop" target="_blank" rel="noopener noreferrer"><i class="bi bi-twitter-x text-light"></i></a>
+            <a className="nav-link d-inline p-2 text-decoration-none" href="https://www.facebook.com/redbullshop/" target="_blank" rel="noopener noreferrer"><i className="bi bi-facebook text-light"></i></a>
+            <a className="nav-link d-inline p-2 text-decoration-none" href="https://www.instagram.com/redbullshop/" target="_blank" rel="noopener noreferrer"><i className="bi bi-instagram text-light"></i></a>
+            <a className="nav-link d-inline p-2 text-decoration-none" href="https://twitter.com/redbullshop" target="_blank" rel="noopener noreferrer"><i className="bi bi-twitter-x text-light"></i></a>
         </li>
     )
 }
